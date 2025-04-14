1eea8f2c1e0e3feaab7c72108e83abe982bac63d
Ayush <thakareayush12@gmail.com>
2025-04-15 00:30:14 +0530

    first commit

---
+++ Project/client/src/utils/landing_page_developers.js
@@ -0,0 +1,47 @@
+const Images = [
+    {
+        id: 0,
+        src: "/img/landing_page_images/features-01.png",
+        title: "feature1",
+    },
+    {
+        id: 1,
+        src: "/img/landing_page_images/features-02.png",
+        title: "feature2",
+    },
+    {
+        id: 2,
+        src: "/img/landing_page_images/features-03.png",
+        title: "feature3",
+    },
+    {
+        id: 3,
+        title: "Ayush Thakare",
+        role: "FullStack Dev",
+        linkedinUrl: "https://www.linkedin.com/in/ayush-thakare-833941251/",
+        githubUrl: "https://github.com/Ayush12007",
+    },
+    {
+        id: 4,
+        title: "Nehil Nama",
+        role: "FullStack Dev",
+        linkedinUrl: "https://www.linkedin.com/in/nehil-nama-772972247/",
+        githubUrl: "https://github.com/Nehil-nama",
+    },
+    {
+        id: 5,
+        title: "Divyansh Singhal",
+        role: "FullStack Dev",
+        linkedinUrl: "https://www.linkedin.com/in/divyansh-singhal-83b848250/",
+        githubUrl: "https://github.com/BlueMishu",
+    },
+    {
+        id: 6,
+        title: "Akshat Raj",
+        role: "Product Management",
+        linkedinUrl: "https://www.linkedin.com/in/akshat-raj-88a578250/",
+        githubUrl: "https://github.com/akshat100803",
+    },
+];
+
+export default Images;
